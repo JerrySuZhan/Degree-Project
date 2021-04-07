c51f9dfdd056d89ae0ebbddcd8b1aaf7a4d41145
Su Zhan <suzhan38@126.com>
2021-04-07 15:02:33 +0800

    single property

--- blogapp/routes.py
+++ blogapp/routes.py
@@ -34,143 +34,69 @@ def index():
 @app.route('/listing/<int:page><string:name><string:district><string:bedrooms><string:livingrooms><string:price_per_meter>',
            methods=['post', 'get'])
 def listing(page=None, name=None, district=None, bedrooms=None, livingrooms=None, price_per_meter=None):
-    print("成功1")
-    if not page:
-        page = 1
-    if not name:
-        name = 'null'
-    if not district:
-        district = 'null'
-    if not bedrooms:
-        bedrooms = 'null'
-    if not livingrooms:
-        livingrooms = 'null'
-    if not price_per_meter:
-        price_per_meter = 'null'
+    if not session.get("LOGING_USER") is None:
 
-    if request.method == 'POST':
-        search_input = request.form.get('content')  # 需要查询的内容
-        district_input = request.form.get('district')
-        bedrooms_input = request.form.get('bedrooms')
-        livingrooms_input = request.form.get('livingrooms')
-        price_per_meter_input = request.form.get('price_per_meter')
+        print("成功1")
+        if not page:
+            page = 1
+        if not name:
+            name = 'null'
+        if not district:
+            district = 'null'
+        if not bedrooms:
+            bedrooms = 'null'
+        if not livingrooms:
+            livingrooms = 'null'
+        if not price_per_meter:
+            price_per_meter = 'null'
 
-        session['name'] = search_input
-        session['district'] = district_input
-        session['bedrooms'] = bedrooms_input
-        session['livingrooms'] = livingrooms_input
-        session['price_per_meter'] = price_per_meter_input
-        # if search_input is None:
-        #     search_input = ""
-        #     session['name'] = search_input
-        # if district_input is None or district_input == "Any":
-        #     district_input = ""
-        #     session['district'] = district_input
-        # if bedrooms_input is None or bedrooms_input == "Any":
-        #     bedrooms_input = ""
-        #     session['bedrooms'] = bedrooms_input
-        # if livingrooms_input is None or livingrooms_input == "Any":
-        #     livingrooms_input = ""
-        #     session['livingrooms'] = livingrooms_input
+        if request.method == 'POST':
+            search_input = request.form.get('content')  # 需要查询的内容
+            district_input = request.form.get('district')
+            bedrooms_input = request.form.get('bedrooms')
+            livingrooms_input = request.form.get('livingrooms')
+            price_per_meter_input = request.form.get('price_per_meter')
 
-        if search_input is None or search_input == "Any" or search_input == "":
-            search_input = ""
             session['name'] = search_input
-        if district_input is None or district_input == "Any" or district_input == "":
-            district_input = ""
             session['district'] = district_input
-        if bedrooms_input is None or bedrooms_input == "Any" or bedrooms_input == "":
-            bedrooms_input = ""
             session['bedrooms'] = bedrooms_input
-        if livingrooms_input is None or livingrooms_input == "Any" or livingrooms_input == "":
-            livingrooms_input = ""
             session['livingrooms'] = livingrooms_input
-        if price_per_meter_input is None or price_per_meter_input == "Any" or price_per_meter_input == "":
-            price_per_meter_input = ""
             session['price_per_meter'] = price_per_meter_input
-
-
-        print("name1: " + session['name'])
-        print("district1: " + session['district'])
-        print("bedrooms1: " + session['bedrooms'])
-        print("livingrooms1: " + session['livingrooms'])
-        print("price_per_meter1: " + session['price_per_meter'])
-
-        u = Buildings.query.filter(and_
-            (
-            or_(
-                Buildings.district.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.address.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.total_price.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.price_per_meter.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.build_time.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.num_of_bedrooms.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.num_of_living_rooms.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.size.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.orientation.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.building_type.like("%" + search_input + "%") if search_input is not None else "",
-                Buildings.num_of_floors.like("%" + search_input + "%") if search_input is not None else ""
-            ),
-            and_(
-                Buildings.district.like("%" + district_input + "%") if district_input is not None else "",
-                Buildings.num_of_bedrooms.like("%" + bedrooms_input + "%") if bedrooms_input is not None else "",
-                Buildings.num_of_living_rooms.like("%" + livingrooms_input + "%") if livingrooms_input is not None else "",
-                # Buildings.price_per_meter.like("%" + price_per_meter_input + "%") if price_per_meter_input is not None else ""
-        )
-        )
-        )
-        u = u.paginate(page=page, per_page=18)
-        # print("search成功")
-
-        if search_input == "":
-            search_input = 'null'
-        if district_input == "":
-            district_input = 'null'
-        if bedrooms_input == "":
-            bedrooms_input = 'null'
-        if livingrooms_input == "":
-            livingrooms_input = 'null'
-        if price_per_meter_input == "":
-            price_per_meter_input = 'null'
-
-        return render_template('listing.html', u=u.items, pagination=u, search_input=search_input,
-                               district_input=district_input,
-                               bedrooms_input=bedrooms_input, livingrooms_input=livingrooms_input, price_per_meter_input=price_per_meter_input)
-
-    else:
-        print("成功2")
-        if session['name'] == "" and session['district'] == "" and session['bedrooms'] == "" and session['livingrooms'] and session['price_per_meter']== "":
-            print("name: " + session['name'])
-            print("district: " + session['district'])
-            print("bedrooms: " + session['bedrooms'])
-            print("livingrooms: " + session['livingrooms'])
-            print("price_per_meter: " + session['price_per_meter'])
-            search_input = 'null'
-            district_input = 'null'
-            bedrooms_input = 'null'
-            livingrooms_input = 'null'
-            price_per_meter_input = 'null'
-            u = Buildings.query.paginate(page=page, per_page=18)
-        else:
-            search_input = session['name']
-            district_input = session['district']
-            bedrooms_input = session['bedrooms']
-            livingrooms_input = session['livingrooms']
-            price_per_meter_input = session['price_per_meter']
-            # if session['name'] == "":
+            # if search_input is None:
             #     search_input = ""
-            # if session['district'] == "":
+            #     session['name'] = search_input
+            # if district_input is None or district_input == "Any":
             #     district_input = ""
-            # if session['bedrooms'] == "":
+            #     session['district'] = district_input
+            # if bedrooms_input is None or bedrooms_input == "Any":
             #     bedrooms_input = ""
-            # if session['livingrooms'] == "":
+            #     session['bedrooms'] = bedrooms_input
+            # if livingrooms_input is None or livingrooms_input == "Any":
             #     livingrooms_input = ""
+            #     session['livingrooms'] = livingrooms_input
 
-            print("name2: " + session['name'])
-            print("district2: " + session['district'])
-            print("bedrooms2: " + session['bedrooms'])
-            print("livingrooms2: " + session['livingrooms'])
-            print("price_per_meter: " + session['price_per_meter'])
+            if search_input is None or search_input == "Any" or search_input == "":
+                search_input = ""
+                session['name'] = search_input
+            if district_input is None or district_input == "Any" or district_input == "":
+                district_input = ""
+                session['district'] = district_input
+            if bedrooms_input is None or bedrooms_input == "Any" or bedrooms_input == "":
+                bedrooms_input = ""
+                session['bedrooms'] = bedrooms_input
+            if livingrooms_input is None or livingrooms_input == "Any" or livingrooms_input == "":
+                livingrooms_input = ""
+                session['livingrooms'] = livingrooms_input
+            if price_per_meter_input is None or price_per_meter_input == "Any" or price_per_meter_input == "":
+                price_per_meter_input = ""
+                session['price_per_meter'] = price_per_meter_input
+
+
+            print("name1: " + session['name'])
+            print("district1: " + session['district'])
+            print("bedrooms1: " + session['bedrooms'])
+            print("livingrooms1: " + session['livingrooms'])
+            print("price_per_meter1: " + session['price_per_meter'])
 
             u = Buildings.query.filter(and_
                 (
@@ -192,10 +118,11 @@ def listing(page=None, name=None, district=None, bedrooms=None, livingrooms=None
                     Buildings.num_of_bedrooms.like("%" + bedrooms_input + "%") if bedrooms_input is not None else "",
                     Buildings.num_of_living_rooms.like("%" + livingrooms_input + "%") if livingrooms_input is not None else "",
                     # Buildings.price_per_meter.like("%" + price_per_meter_input + "%") if price_per_meter_input is not None else ""
-                )
+            )
             )
             )
             u = u.paginate(page=page, per_page=18)
+            # print("search成功")
 
             if search_input == "":
                 search_input = 'null'
@@ -208,32 +135,124 @@ def listing(page=None, name=None, district=None, bedrooms=None, livingrooms=None
             if price_per_meter_input == "":
                 price_per_meter_input = 'null'
 
-        print("未进行search")
-        return render_template('listing.html', u=u.items, pagination=u, search_input=search_input,
-                               district_input=district_input,
-                               bedrooms_input=bedrooms_input, livingrooms_input=livingrooms_input, price_per_meter_input=price_per_meter_input)
+            return render_template('listing.html', u=u.items, pagination=u, search_input=search_input,
+                                   district_input=district_input,
+                                   bedrooms_input=bedrooms_input, livingrooms_input=livingrooms_input, price_per_meter_input=price_per_meter_input)
+
+        else:
+            print("成功2")
+            if session['name'] == "" and session['district'] == "" and session['bedrooms'] == "" and session['livingrooms'] and session['price_per_meter']== "":
+                print("name: " + session['name'])
+                print("district: " + session['district'])
+                print("bedrooms: " + session['bedrooms'])
+                print("livingrooms: " + session['livingrooms'])
+                print("price_per_meter: " + session['price_per_meter'])
+                search_input = 'null'
+                district_input = 'null'
+                bedrooms_input = 'null'
+                livingrooms_input = 'null'
+                price_per_meter_input = 'null'
+                u = Buildings.query.paginate(page=page, per_page=18)
+            else:
+                search_input = session['name']
+                district_input = session['district']
+                bedrooms_input = session['bedrooms']
+                livingrooms_input = session['livingrooms']
+                price_per_meter_input = session['price_per_meter']
+                # if session['name'] == "":
+                #     search_input = ""
+                # if session['district'] == "":
+                #     district_input = ""
+                # if session['bedrooms'] == "":
+                #     bedrooms_input = ""
+                # if session['livingrooms'] == "":
+                #     livingrooms_input = ""
+
+                print("name2: " + session['name'])
+                print("district2: " + session['district'])
+                print("bedrooms2: " + session['bedrooms'])
+                print("livingrooms2: " + session['livingrooms'])
+                print("price_per_meter: " + session['price_per_meter'])
+
+                u = Buildings.query.filter(and_
+                    (
+                    or_(
+                        Buildings.district.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.address.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.total_price.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.price_per_meter.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.build_time.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.num_of_bedrooms.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.num_of_living_rooms.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.size.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.orientation.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.building_type.like("%" + search_input + "%") if search_input is not None else "",
+                        Buildings.num_of_floors.like("%" + search_input + "%") if search_input is not None else ""
+                    ),
+                    and_(
+                        Buildings.district.like("%" + district_input + "%") if district_input is not None else "",
+                        Buildings.num_of_bedrooms.like("%" + bedrooms_input + "%") if bedrooms_input is not None else "",
+                        Buildings.num_of_living_rooms.like("%" + livingrooms_input + "%") if livingrooms_input is not None else "",
+                        # Buildings.price_per_meter.like("%" + price_per_meter_input + "%") if price_per_meter_input is not None else ""
+                    )
+                )
+                )
+                u = u.paginate(page=page, per_page=18)
+
+                if search_input == "":
+                    search_input = 'null'
+                if district_input == "":
+                    district_input = 'null'
+                if bedrooms_input == "":
+                    bedrooms_input = 'null'
+                if livingrooms_input == "":
+                    livingrooms_input = 'null'
+                if price_per_meter_input == "":
+                    price_per_meter_input = 'null'
+
+            print("未进行search")
+            return render_template('listing.html', u=u.items, pagination=u, search_input=search_input,
+                                   district_input=district_input,
+                                   bedrooms_input=bedrooms_input, livingrooms_input=livingrooms_input, price_per_meter_input=price_per_meter_input)
+
+    else:
+        flash('User should login first!')
+        return redirect(url_for('login'))
+
     return render_template('listing.html')
 
 
-@app.route('/single')
-def single():
-    return render_template('single-property-1.html')
+@app.route('/single/<int:id>', methods=['GET', 'POST'])
+def single(id=None):
+    if not session.get("LOGING_USER") is None:
+        single_property = Buildings.query.filter(Buildings.id == id).first()
+        return render_template('single-property-1.html', single_property = single_property)
+
+    else:
+        flash('User should login first!')
+        return redirect(url_for('login'))
+
 
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
-    form = AddHouseForm()
-    # if form.validate_on_submit():
-    if request.method == 'POST':
-        building = Buildings(district=form.district.data, address=form.address.data, total_price=form.total_price.data,
-                             price_per_meter=form.total_price.data * 10000 / form.size.data, build_time=form.year.data,
-                             num_of_bedrooms=form.bedroom_number.data,
-                             num_of_living_rooms=form.livingroom_number.data, size=form.size.data,
-                             orientation=form.orientation.data,
-                             building_type=form.floor_range.data, num_of_floors=form.floor_number.data)
-        db.session.add(building)
-        db.session.commit()
-        return redirect(url_for('index'))
+    if not session.get("LOGING_USER") is None:
+        form = AddHouseForm()
+        # if form.validate_on_submit():
+        if request.method == 'POST':
+            building = Buildings(district=form.district.data, address=form.address.data, total_price=form.total_price.data,
+                                 price_per_meter=form.total_price.data * 10000 / form.size.data, build_time=form.year.data,
+                                 num_of_bedrooms=form.bedroom_number.data,
+                                 num_of_living_rooms=form.livingroom_number.data, size=form.size.data,
+                                 orientation=form.orientation.data,
+                                 building_type=form.floor_range.data, num_of_floors=form.floor_number.data)
+            db.session.add(building)
+            db.session.commit()
+            return redirect(url_for('index'))
+    else:
+        flash('User should login first!')
+        return redirect(url_for('login'))
+
     return render_template('post.html', form=form)
 
 
